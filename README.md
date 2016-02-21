af111a7df08c6679b7a941ba2aaca260d585e990
Tiago Porto <tiagoporto@ymail.com>
2016-02-21 11:50:55 -0300

    Starting implement getclass

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # jquery-svg-to-inline
-jQuery plugin put svg inline
+
+> Jquery plugin to embbed svg inline.
+
+## Example
+
+```html
+<object class="svg" type="image/svg+xml" data="htts://somesvg.svg"></object>
+```
+
+or
+```html
+<img src="https://somesvg.svg"  class="svg">
+```
+
+Will generate
+
+```html
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <circle cx="50" cy="50" r="50"/>
+</svg>
+```
+
+
+## Usage
+
+```js
+$( ".svg" ).svgToInline();
+```
---
+++ jquery-svg-to-inline.js
@@ -0,0 +1,61 @@
+/*!	jQuery SVG to Inline v0.0.1beta
+*	https://github.com/tiagoporto/jquery-svg-to-inline
+*	Copyright (c) 2015-2016 Tiago Porto (tiagoporto.com)
+*	Released under the MIT license
+*/
+
+$.fn.svgToInline = function() {
+	this.each(function() {
+		var path = $(this).attr('data') || $(this).attr('src');
+		var current = $(this);
+		var getClass = [];
+		var elementClass = '';
+
+
+		$($(this).attr('class').split(' ')).each(function() {
+			if (this.toString() !== 'svg') {
+				getClass.push( this.toString() );
+			};
+		});
+
+
+		if(getClass.length > 0){
+
+			for (var i = 0; i < getClass.length; ++i) {
+				var space = '';
+
+				if (i !== getClass.length - 1) {
+				  space = ' ';
+				}
+
+				elementClass += getClass[i] + space;
+
+			}
+		}
+
+		// console.log( '+' + elementClass  + '+');
+
+
+		$.ajax({
+			url : path,
+			dataType: "text",
+			success : function (data) {
+				var element = data.replace(/<[?!][\s\w\"-\/:=?]+>/g, '');
+
+
+				// var getSvgTag = element.match(/<svg[\w\s\t\n:="\\'\/.#-]+>/g);
+
+
+				// var addedClass = getSvgTag[0].replace('>', ' class="' + getClass + '">');
+
+
+				// var newElement = element.replace(/<svg[\w\s\t\n:="\\'\/.#-]+>/g, addedClass);
+
+				// console.log(newElement);
+
+				current.replaceWith( element );
+			}
+		});
+
+	});
+};
