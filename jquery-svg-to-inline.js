bc9e6af7ca59da91a19fc0e6e8f94a2125c733b5
Tiago Porto <tiagoporto@ymail.com>
2016-02-29 23:42:43 -0300

    Fix lint errors

--- jquery-svg-to-inline.js
+++ jquery-svg-to-inline.js
@@ -1,58 +1,58 @@
-/*!	jQuery SVG to Inline v0.1.0beta
+/*!	jQuery SVG to Inline v0.1.1beta
 *	https://github.com/tiagoporto/jquery-svg-to-inline
 *	Copyright (c) 2015-2016 Tiago Porto (tiagoporto.com)
 *	Released under the MIT license
 */
 
-$.fn.svgToInline = function() {
+$.fn.svgToInline = function () {
+	'use strict';
+
 	var usedClass = this.selector;
 
-	this.each(function() {
+	this.each(function () {
 		var path = $(this).attr('data') || $(this).attr('src');
 		var current = $(this);
 		var getClass = [];
 		var newElementClass = '';
 
-		$($(this).attr('class').split(' ')).each(function() {
+		$($(this).attr('class').split(' ')).each(function () {
 			if (this.toString() !== usedClass.replace('.', '')) {
-				getClass.push( this.toString() );
-			};
+				getClass.push(this.toString());
+			}
 		});
 
-		if(getClass.length > 0){
+		if (getClass.length > 0) {
 			var elementClass = '';
 
 			for (var i = 0; i < getClass.length; ++i) {
 				var space = '';
 
 				if (i !== getClass.length - 1) {
-				  space = ' ';
+					space = ' ';
 				}
 
 				elementClass += getClass[i] + space;
-
 			}
 		}
 
-		if(elementClass !== ''){
-			newElementClass = `class="${elementClass}"`;
-		};
+		if (elementClass !== '') {
+			newElementClass = 'class="' + elementClass + '"';
+		}
 
 		$.ajax({
-			url : path,
-			dataType: "text",
-			success : function (data) {
+			url: path,
+			dataType: 'text',
+			success: function success(data) {
 				var element = data.replace(/<[?!][\s\w\"-\/:=?]+>/g, '');
 
 				var getSvgTag = element.match(/<svg[\w\s\t\n:="\\'\/.#-]+>/g);
 
-				var addedClass = getSvgTag[0].replace('>', `${newElementClass}>`);
+				var addedClass = getSvgTag[0].replace('>', newElementClass + '>');
 
 				var newElement = element.replace(/<svg[\w\s\t\n:="\\'\/.#-]+>/g, addedClass);
 
-				current.replaceWith( newElement );
+				current.replaceWith(newElement);
 			}
 		});
-
 	});
 };
